aba3b8d0e071cc303f8f0c24b337563e9dbd72b4
DaichiNakamura <nkc.daichi.nakamura@gmail.com>
2021-12-14 16:56:42 +0900

    feat: add hogehoge Script

---
+++ Zemi211214/Assets/hogehoge.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hogehoge : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
